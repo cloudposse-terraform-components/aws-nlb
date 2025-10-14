ac2d1cac6c52a27ac953770e8ab781e29d85ed37
milldr <miller0daniel@gmail.com>
2025-10-14 11:37:04 -0400

    added arn to output

--- src/outputs.tf
+++ src/outputs.tf
@@ -2,3 +2,8 @@ output "nlb" {
   description = "The NLB of the Component"
   value       = one(module.nlb[*])
 }
+
+output "nlb_arn" {
+  description = "The ARN of the NLB"
+  value       = one(module.nlb[*].arn)
+}
